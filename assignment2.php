74fc10baddfdb9936f8cfc5358e54496cbdc95a4
Almasfatima792 <67876112+Almasfatima792@users.noreply.github.com>
2020-07-10 10:00:48 +0500

    added assignment2.php

---
+++ assignment2.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="utf-8">
+     <meta name="decription" content="assignment">
+	   <title> assignment </title>
+	    </head>
+		<body>
+<?php
+	 $price=array(
+	 'mango'=> '50rs.',
+	 'banana'=>'80rs.',
+	 'orange'=>'90rs.'
+	 );
+	 echo 'price of mango is ' .$price['mango'] ."<br>";
+	 echo 'price of orange is ' .$price['orange'] ."<br>";
+	 echo 'price of banana is ' .$price['banana'] ."<br>";
+		 ?>
+		
+			 </body>
+			  </html>
